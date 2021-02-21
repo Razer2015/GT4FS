f1fc5d91ed59cc40f0eecfc8f3fa56e479a08b48
Nenkai <neenkaai@gmail.com>
2021-02-22 00:51:21 +0100

    Index Block Serializing.

--- GT4FS.Core/Packing/TocBuilder.cs
+++ GT4FS.Core/Packing/TocBuilder.cs
@@ -34,6 +34,7 @@ namespace GT4FS.Core.Packing
 
         // List of all TOC block buffers, kept in memory to write the previous/next pages and page offsets later on.
         public List<byte[]> _entryBlocks;
+        public List<byte[]> _indexBlocks;
 
         /// <summary>
         /// Current node ID.
@@ -86,12 +87,31 @@ namespace GT4FS.Core.Packing
             volStream.BaseStream.Position = 0x800 + TocHeader.HeaderSize;
 
             // Write all the data blocks.
-            WriteDataBlocks();
+            WriteEntryMetaBlocks();
 
-            // TODO: Write Index blocks based on the data blocks.
+            // Write Index blocks based on the data blocks.
+            _currentEntry = 0;
+            WriteIndexBlocks();
+
+            // Fill up index block's next/previous now that they've been correctly made
+            FillIndexBlockLinks();
+
+            // We got the index block count, so we can write the next/previous for our entries too
+            FillEntryMetaBlockLinks();
+
         }
 
-        private byte[] WriteNextBlock()
+        public void WriteIndexBlocks()
+        {
+            _indexBlocks = new List<byte[]>();
+            while (_currentEntry < _firstBlockEntries.Count)
+            {
+                byte[] dataBlock = WriteNextIndexBlock();
+                _indexBlocks.Add(dataBlock);
+            }
+        }
+
+        private byte[] WriteNextIndexBlock()
         {
             byte[] buffer = new byte[BlockSize];
             SpanWriter blockWriter = new SpanWriter(buffer);
@@ -100,7 +120,91 @@ namespace GT4FS.Core.Packing
             blockWriter.Position = BlockHeaderSize;
             ushort entriesWriten = 0;
 
-            int blockSpaceLeft = BlockSize - BlockHeaderSize;
+            int blockSpaceLeft = BlockSize - BlockHeaderSize + 4; // Account for the int at the begining of the bottom toc
+
+            while (true)
+            {
+                if (_currentEntry >= _firstBlockEntries.Count)
+                    break; // We are completely done writing the TOC, finish up the block
+
+                Entry entry = _firstBlockEntries[_currentEntry];
+                int entrySize = 4 + Encoding.UTF8.GetByteCount(entry.Name);
+                if (entrySize > blockSpaceLeft)
+                    break; // Predicted exceeded block bound, finish it up
+
+                // Begin to write the entry's common information
+                int entryOffset = blockWriter.Position;
+                blockWriter.Endian = Endian.Big;
+                blockWriter.WriteInt32(entry.ParentNode);
+                blockWriter.Endian = Endian.Little;
+                blockWriter.WriteString0(entry.Name);
+                blockWriter.Align(0x04); // String is aligned
+
+                int endPos = blockWriter.Position;
+
+                blockSpaceLeft -= entrySize + 0x08; // Include the block's toc entry
+
+                // Write the lookup information at the end of the block
+                blockWriter.Position = BlockSize - ((entriesWriten + 1) * 0x08);
+                blockWriter.WriteUInt16((ushort)entryOffset);
+                blockWriter.WriteUInt16((ushort)(4 + Encoding.UTF8.GetByteCount(entry.Name)));
+                blockWriter.WriteUInt32(0); // We will write the block index later as we don't have it
+
+                // Move on to next.
+                entriesWriten++;
+                _currentEntry++;
+
+                blockWriter.Position = endPos;
+            }
+
+            // Write up the block info - write what we can write - the entry count
+            blockWriter.Position = 0;
+            blockWriter.WriteInt16(1); // Block Type - Index
+            blockWriter.WriteUInt16((ushort)((entriesWriten * 2) + 1));
+
+            return buffer;
+        }
+
+        private void FillIndexBlockLinks()
+        {
+            for (int i = 0; i < _indexBlocks.Count; i++)
+            {
+                SpanWriter block = new SpanWriter(_indexBlocks[i]);
+
+                // Next page
+                block.Position = 4;
+                block.WriteInt32(i != _indexBlocks.Count - 1 ? i + 1 : -1);
+
+                // Previous page
+                block.WriteInt32(i != 0 ? i - 1 : -1);
+
+            }
+        }
+
+        /// <summary>
+        /// Serializes all the entries into toc data blocks.
+        /// </summary>
+        /// <returns></returns>
+        private void WriteEntryMetaBlocks()
+        {
+            _entryBlocks = new List<byte[]>();
+            while (_currentEntry < _entries.Count)
+            {
+                byte[] dataBlock = WriteNextEntryMetaBlock();
+                _entryBlocks.Add(dataBlock);
+            }
+        }
+
+        private byte[] WriteNextEntryMetaBlock()
+        {
+            byte[] buffer = new byte[BlockSize];
+            SpanWriter blockWriter = new SpanWriter(buffer);
+
+            // Skip block header for now
+            blockWriter.Position = BlockHeaderSize;
+            ushort entriesWriten = 0;
+
+            int blockSpaceLeft = BlockSize - BlockHeaderSize + 4; // Account for the int at the begining of the bottom toc
 
             while (true)
             {
@@ -128,18 +232,17 @@ namespace GT4FS.Core.Packing
                 entry.SerializeTypeMeta(ref blockWriter);
                 blockWriter.Align(0x04); // Whole entry is also aligned
 
-
                 int endPos = blockWriter.Position;
 
                 blockSpaceLeft -= entrySize + 0x08; // Include the block's toc entry
 
                 // Write the lookup information at the end of the block
-                blockWriter.Position = BlockSize - ((entriesWriten+1) * 0x08);
+                blockWriter.Position = BlockSize - ((entriesWriten + 1) * 0x08);
                 blockWriter.WriteUInt16((ushort)entryOffset);
                 blockWriter.WriteUInt16((ushort)entry.GetEntryMetaSize());
                 blockWriter.WriteUInt16((ushort)entryMetaOffset);
                 blockWriter.WriteUInt16(entry.GetTypeMetaSize());
-                
+
                 // Move on to next.
                 entriesWriten++;
                 _currentEntry++;
@@ -150,23 +253,30 @@ namespace GT4FS.Core.Packing
             // Write up the block info - write what we can write - the entry count
             blockWriter.Position = 0;
             blockWriter.WriteInt16(0); // Block Type
-            blockWriter.WriteUInt16(entriesWriten);
+            blockWriter.WriteUInt16((ushort)((entriesWriten * 2) + 1));
 
             return buffer;
         }
 
-        /// <summary>
-        /// Serializes all the entries into toc data blocks.
-        /// </summary>
-        /// <returns></returns>
-        private void WriteDataBlocks()
+        private void FillEntryMetaBlockLinks()
         {
-            _entryBlocks = new List<byte[]>();
-            while (_currentEntry < _entries.Count)
+            for (int i = 0; i < _entryBlocks.Count; i++)
             {
-                byte[] dataBlock = WriteNextBlock();
-                _entryBlocks.Add(dataBlock);
+                SpanWriter block = new SpanWriter(_entryBlocks[i]);
+
+                // Next page
+                block.Position = 4;
+                block.WriteInt32(i != _entryBlocks.Count - 1 ? _indexBlocks.Count + (i + 1) : -1);
+
+                // Previous page
+                block.WriteInt32(i != 0 ? _indexBlocks.Count + (i - 1) : -1);
+
             }
+        }
+
+        private void FillEntryMetaBlockIndexes()
+        {
+
         }
 
         /// <summary>
